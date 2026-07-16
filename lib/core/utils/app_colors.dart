9e6ec0fd1d39c4ca64ddf4f5a9cd1f15674f878a
Nady Alwazah <nadysy.7@gmail.com>
2026-07-17 02:56:18 +0300

    feat: add AppColors with primary, background and text colors.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF7F56D9);
+  static const Color backgroundColor = Color(0xFFFCFCFD);
+  static const Color textPrimary = Color(0xFF1F2A37);
+  static const Color textSecondary = Color(0xFF9DA4AE);
+}
