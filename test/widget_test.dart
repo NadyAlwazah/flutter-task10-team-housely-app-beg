c267dd381e3b4fd3b98c0bc9c6559740454dc7c1
Nady Alwazah <nadysy.7@gmail.com>
2026-07-17 01:49:10 +0300

    chore: add core dependencies (screenutil, google_fonts, svg, go_router, bloc, prefs, cache, slidable)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_task10_team_housely_app_beg/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HouselyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
