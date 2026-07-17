8d7e49e63c24799d0663486f9004977fb535fee5
Nady Alwazah <nadysy.7@gmail.com>
2026-07-17 03:49:05 +0300

    feat: create Styles class with reusable text styles for the entire project

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/utils/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Styles {
+  static TextStyle get textStyle24W800Inter => GoogleFonts.inter(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w800,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle24W600Inter => GoogleFonts.inter(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle20W600Inter => GoogleFonts.inter(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle18W400Inter =>
+      GoogleFonts.inter(fontSize: 18.sp, fontWeight: FontWeight.w400);
+
+  static TextStyle get textStyle16W500Inter =>
+      GoogleFonts.inter(fontSize: 16.sp, fontWeight: FontWeight.w500);
+
+  static TextStyle get textStyle16W600Inter => GoogleFonts.inter(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle14W400Inter => GoogleFonts.inter(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+
+  static TextStyle get textStyle14W500Inter => GoogleFonts.inter(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle14W600Inter => GoogleFonts.inter(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle14W700Inter =>
+      GoogleFonts.inter(fontSize: 14.sp, fontWeight: FontWeight.w700);
+
+  static TextStyle get textStyle12W500Inter => GoogleFonts.inter(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle12W400Inter => GoogleFonts.inter(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+
+  static TextStyle get textStyle12W600Inter => GoogleFonts.inter(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle10W600Inter => GoogleFonts.inter(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get textStyle10W400Inter => GoogleFonts.inter(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+}
