41ec5d98ab9674774f92d395c899d559a5947682
Nady Alwazah <nadysy.7@gmail.com>
2026-07-18 16:52:57 +0300

    feat: add reusable CustomAppBar and integrate it into SignupViewBody

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/widgets/custom_app_bar.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/auth/presentation/views/widgets/signup_view_body.dart';
+import 'package:go_router/go_router.dart';
 
 class SignupView extends StatelessWidget {
   const SignupView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SignupViewBody());
+    return Scaffold(
+      appBar: CustomAppBar(onTapLeading: () => context.pop()),
+      body: const SignupViewBody(),
+    );
   }
 }
