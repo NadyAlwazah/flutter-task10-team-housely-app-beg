379fa5dc0241b3ee06d678827e62f752fcd6275b
haninhamoudi96-hub <haninhamoudi96@gmail.com>
2026-07-19 23:26:43 +0300

    Select Location done

--- lib/features/select_location/data/location_search_service.dart
+++ lib/features/select_location/data/location_search_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class LocationResult {
@@ -15,7 +14,9 @@ class LocationResult {
 }
 
 class LocationSearchService {
-  Future<List<LocationResult>> searchLocation(String query) async {
+  Future<List<LocationResult>> searchLocation(
+    String query,
+  ) async {
     final url = Uri.parse(
       'https://nominatim.openstreetmap.org/search'
       '?q=${Uri.encodeQueryComponent(query)}'
@@ -36,12 +37,39 @@ class LocationSearchService {
 
     final List data = jsonDecode(response.body);
 
-    return data.map<LocationResult>((item) {
+    return data.map((item) {
       return LocationResult(
         displayName: item['display_name'],
         latitude: double.parse(item['lat']),
         longitude: double.parse(item['lon']),
       );
     }).toList();
+  }
+
+  Future<String> getAddressFromCoordinates(
+    double latitude,
+    double longitude,
+  ) async {
+    final url = Uri.parse(
+      'https://nominatim.openstreetmap.org/reverse'
+      '?lat=$latitude'
+      '&lon=$longitude'
+      '&format=json',
+    );
+
+    final response = await http.get(
+      url,
+      headers: {
+        'User-Agent': 'Team-Housely-Flutter-App',
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Reverse geocoding failed');
+    }
+
+    final data = jsonDecode(response.body);
+
+    return data['display_name'] ?? 'Unknown location';
   }
 }
