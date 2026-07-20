53fa30d7c80d4981ba4fbc9ff555ad6a9b4c56e2
Nady Alwazah <nadysy.7@gmail.com>
2026-07-21 02:54:43 +0300

    feat: add reusable SectionHeader widget and integrate it into ResetPasswordViewBody

--- lib/features/auth/presentation/views/widgets/reset_password_view_body.dart
+++ lib/features/auth/presentation/views/widgets/reset_password_view_body.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task10_team_housely_app_beg/features/auth/presentation/views/widgets/section_header.dart';
 
 class ResetPasswordViewBody extends StatelessWidget {
   const ResetPasswordViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        children: [
+          SectionHeader(
+            title: "Forgot Password",
+            subTitle:
+                "Select which contact details should we use to reset your password ",
+          ),
+        ],
+      ),
+    );
   }
 }
