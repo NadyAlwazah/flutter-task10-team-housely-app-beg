3bdcf154f151df1883a49bac7658d839f81a6046
haninhamoudi96-hub <haninhamoudi96@gmail.com>
2026-07-20 19:47:16 +0300

    Map services has updated

--- lib/features/select_location/data/manager/location_cubit.dart
+++ lib/features/select_location/data/manager/location_cubit.dart
@@ -1,21 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_task10_team_housely_app_beg/core/services/service_locator.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/auth/data/data_sources/auth_local_data_source.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/auth/data/models/user_model.dart';
 
 class LocationCubit extends Cubit<String?> {
-  LocationCubit() : super(null);
-  final AuthLocalDataSource _local = getIt<AuthLocalDataSource>();
+  final AuthLocalDataSource _dataSource;
+
+  LocationCubit(this._dataSource) : super(null);
+
   Future<void> updateLocation(String newLocation) async {
     try {
-      final user = await _local.getUser();
+      final UserModel? user = await _dataSource.getUser();
 
       if (user == null) {
         emit(null);
         return;
       }
 
-      final updatedUser = UserModel(
+      final UserModel updatedUser = UserModel(
         fullName: user.fullName,
         email: user.email,
         rememberMe: user.rememberMe,
@@ -23,7 +24,7 @@ class LocationCubit extends Cubit<String?> {
         myLocation: newLocation,
       );
 
-      await _local.saveUser(updatedUser);
+      await _dataSource.saveUser(updatedUser);
 
       emit(newLocation);
     } catch (e) {
