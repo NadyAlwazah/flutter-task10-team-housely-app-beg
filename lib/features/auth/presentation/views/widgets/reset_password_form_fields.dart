68fa3f1ad26db9d8260e14cc163384615fa78ea3
Nady Alwazah <nadysy.7@gmail.com>
2026-07-21 14:46:16 +0300

    feat(auth): create ResetPasswordFormFields widget with new and confirm password inputs

---
+++ lib/features/auth/presentation/views/widgets/reset_password_form_fields.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/utils/validators.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/widgets/form_label.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/widgets/custom_text_form_field.dart';
+
+class ResetPasswordFormFields extends StatelessWidget {
+  const ResetPasswordFormFields({
+    super.key,
+    required this.newPasswordController,
+    required this.confirmPasswordController,
+  });
+  final TextEditingController newPasswordController;
+  final TextEditingController confirmPasswordController;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // New Password
+        const FormLabel(text: "New Password"),
+        SizedBox(height: 4.h),
+        CustomTextFormField(
+          controller: newPasswordController,
+          hintText: "Enter new password",
+          textInputType: TextInputType.visiblePassword,
+          obscureText: true,
+          validator: Validators.validatePassword,
+        ),
+
+        SizedBox(height: 16.h),
+
+        // Confirm Password
+        const FormLabel(text: "Confirm Password"),
+        SizedBox(height: 4.h),
+        CustomTextFormField(
+          controller: confirmPasswordController,
+          hintText: "Re-enter password",
+          textInputType: TextInputType.visiblePassword,
+          obscureText: true,
+          validator: Validators.validatePassword,
+        ),
+      ],
+    );
+  }
+}
--- lib/features/auth/presentation/views/widgets/reset_password_view_body.dart
+++ lib/features/auth/presentation/views/widgets/reset_password_view_body.dart
@@ -1,20 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_task10_team_housely_app_beg/features/auth/presentation/views/widgets/reset_password_form_fields.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/auth/presentation/views/widgets/section_header.dart';
 
-class ResetPasswordViewBody extends StatelessWidget {
+class ResetPasswordViewBody extends StatefulWidget {
   const ResetPasswordViewBody({super.key});
 
   @override
+  State<ResetPasswordViewBody> createState() => _ResetPasswordViewBodyState();
+}
+
+class _ResetPasswordViewBodyState extends State<ResetPasswordViewBody> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController newPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    newPasswordController.dispose();
+    confirmPasswordController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(
-        children: [
-          SectionHeader(
-            title: "Create New Password",
-            subTitle: "Please enter a new password to change",
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SectionHeader(
+              title: "Create New Password",
+              subTitle: "Please enter a new password\nto change",
+            ),
+
+            SizedBox(height: 32.h),
+
+            ResetPasswordFormFields(
+              newPasswordController: newPasswordController,
+              confirmPasswordController: confirmPasswordController,
+            ),
+
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
