d4cda4f5105825d3df50dbd173622e48fc51caa8
Nady Alwazah <nadysy.7@gmail.com>
2026-07-24 19:09:46 +0300

    feat(favourite): implement filtering logic to render only properties marked as favorite

--- lib/features/favourite/presentation/views/favourite_view.dart
+++ lib/features/favourite/presentation/views/favourite_view.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/services/service_locator.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/favourite/presentation/views/widgets/favourite_view_body.dart';
+import 'package:flutter_task10_team_housely_app_beg/features/home/data/dummy/property_dummy_data.dart';
+import 'package:flutter_task10_team_housely_app_beg/features/home/data/manager/property_cubit/property_cubit.dart';
 
 class FavouriteView extends StatelessWidget {
   const FavouriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [FavouriteViewBody()]);
+    return BlocProvider(
+      create: (context) => getIt<PropertyCubit>()
+        ..loadData(
+          popular: popularProperties,
+          recommended: recommendedProperties,
+        ),
+      child: const SafeArea(child: FavouriteViewBody()),
+    );
   }
 }
