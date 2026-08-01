2f194858e33661a63deada52f4dfb8cb375bd0d3
Nady Alwazah <nadysy.7@gmail.com>
2026-08-01 16:02:32 +0300

    feat(filter): add Property Type section with reusable checkbox item

--- lib/features/search/presentation/views/widgets/fillter_bottom_sheet.dart
+++ lib/features/search/presentation/views/widgets/fillter_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_task10_team_housely_app_beg/core/utils/styles.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/search/presentation/views/widgets/filter_looking_for_section.dart';
+import 'package:flutter_task10_team_housely_app_beg/features/search/presentation/views/widgets/filter_property_type_section.dart';
 
 class FillterBottomSheet extends StatelessWidget {
   const FillterBottomSheet({super.key});
@@ -19,8 +20,14 @@ class FillterBottomSheet extends StatelessWidget {
         children: [
           SizedBox(height: 39.h),
           Center(child: Text("Fillter", style: Styles.textStyle16W600Inter)),
+
           SizedBox(height: 16.h),
+
           const FilterLookingForSection(),
+
+          SizedBox(height: 24.h),
+
+          const FilterPropertyTypeSection(),
         ],
       ),
     );
---
+++ lib/features/search/presentation/views/widgets/filter_property_type_section.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/utils/app_colors.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/utils/styles.dart';
+import 'package:flutter_task10_team_housely_app_beg/features/search/presentation/views/widgets/filter_checkbox_item.dart';
+
+class FilterPropertyTypeSection extends StatefulWidget {
+  const FilterPropertyTypeSection({super.key});
+
+  @override
+  State<FilterPropertyTypeSection> createState() =>
+      _FilterPropertyTypeSectionState();
+}
+
+class _FilterPropertyTypeSectionState extends State<FilterPropertyTypeSection> {
+  bool apartment = true;
+  bool penthouse = false;
+  bool hotel = true;
+  bool villa = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // العنوان
+        Text("Property Type", style: Styles.textStyle16W600Inter),
+        SizedBox(height: 16.h),
+
+        FilterCheckboxItem(
+          title: "Apartment",
+          value: apartment,
+          onChanged: (val) => setState(() => apartment = val!),
+        ),
+
+        FilterCheckboxItem(
+          title: "Penthouse",
+          value: penthouse,
+          onChanged: (val) => setState(() => penthouse = val!),
+        ),
+
+        FilterCheckboxItem(
+          title: "Hotel",
+          value: hotel,
+          onChanged: (val) => setState(() => hotel = val!),
+        ),
+
+        FilterCheckboxItem(
+          title: "Villa",
+          value: villa,
+          onChanged: (val) => setState(() => villa = val!),
+        ),
+
+        SizedBox(height: 4.h),
+
+        Text(
+          "Show all",
+          style: Styles.textStyle12W600Inter.copyWith(color: AppColors.primary),
+        ),
+      ],
+    );
+  }
+}
