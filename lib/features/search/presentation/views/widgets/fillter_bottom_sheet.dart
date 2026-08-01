2d9b4b2864162487880769ad9c7fc3cebd820195
Nady Alwazah <nadysy.7@gmail.com>
2026-08-02 00:42:41 +0300

    feat(filter): add Price Range section with interactive RangeSlider

--- lib/features/search/presentation/views/widgets/fillter_bottom_sheet.dart
+++ lib/features/search/presentation/views/widgets/fillter_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_task10_team_housely_app_beg/core/utils/styles.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/search/presentation/views/widgets/filter_looking_for_section.dart';
+import 'package:flutter_task10_team_housely_app_beg/features/search/presentation/views/widgets/filter_price_range_section.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/search/presentation/views/widgets/filter_property_type_section.dart';
 
 class FillterBottomSheet extends StatelessWidget {
@@ -11,24 +12,30 @@ class FillterBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 762.h,
-      padding: EdgeInsets.symmetric(horizontal: 24.w),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.vertical(top: Radius.circular(40.r)),
       ),
-      child: Column(
-        children: [
-          SizedBox(height: 39.h),
-          Center(child: Text("Fillter", style: Styles.textStyle16W600Inter)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            SizedBox(height: 39.h),
+            Center(child: Text("Fillter", style: Styles.textStyle16W600Inter)),
 
-          SizedBox(height: 16.h),
+            SizedBox(height: 16.h),
 
-          const FilterLookingForSection(),
+            const FilterLookingForSection(),
 
-          SizedBox(height: 24.h),
+            SizedBox(height: 24.h),
 
-          const FilterPropertyTypeSection(),
-        ],
+            const FilterPropertyTypeSection(),
+
+            SizedBox(height: 24.h),
+
+            const FilterPriceRangeSection(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/search/presentation/views/widgets/filter_price_range_section.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/utils/app_colors.dart';
+import 'package:flutter_task10_team_housely_app_beg/core/utils/styles.dart';
+
+class FilterPriceRangeSection extends StatefulWidget {
+  const FilterPriceRangeSection({super.key});
+
+  @override
+  State<FilterPriceRangeSection> createState() =>
+      _FilterPriceRangeSectionState();
+}
+
+class _FilterPriceRangeSectionState extends State<FilterPriceRangeSection> {
+  RangeValues rangeValues = const RangeValues(0, 1500);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Price Range", style: Styles.textStyle16W600Inter),
+
+        SizedBox(height: 12.h),
+
+        SliderTheme(
+          data: SliderTheme.of(context).copyWith(
+            trackHeight: 2,
+
+            // حجم الدائرتين
+            thumbShape: const RoundSliderThumbShape(
+              enabledThumbRadius: 6,
+              elevation: 0,
+              pressedElevation: 0,
+            ),
+
+            overlayShape: SliderComponentShape.noOverlay,
+
+            activeTrackColor: AppColors.primary,
+            inactiveTrackColor: const Color(0xffD9D9D9),
+
+            rangeThumbShape: const RoundRangeSliderThumbShape(
+              enabledThumbRadius: 6,
+              elevation: 0,
+              pressedElevation: 0,
+            ),
+          ),
+          child: SizedBox(
+            width: double.infinity,
+            child: RangeSlider(
+              values: rangeValues,
+              min: 0,
+              max: 1500,
+              onChanged: (values) {
+                setState(() {
+                  rangeValues = values;
+                });
+              },
+            ),
+          ),
+        ),
+
+        SizedBox(height: 8.h),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "\$${rangeValues.start.toInt()}",
+              style: Styles.textStyle12W500Inter.copyWith(
+                color: AppColors.textSecondary,
+              ),
+            ),
+            Text(
+              "\$${rangeValues.end.toInt()}",
+              style: Styles.textStyle12W500Inter.copyWith(
+                color: AppColors.textSecondary,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
