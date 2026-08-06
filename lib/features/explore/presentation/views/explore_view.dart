164b3a4aeb9c5119178e547778172169d9c45297
Nady Alwazah <nadysy.7@gmail.com>
2026-08-07 00:21:56 +0300

    Revert "Merge pull request #22 from NadyAlwazah/hanin_hamoudi/update_explore"
    This reverts commit e7dc2f78419580eb6a3901821a725fec530a6047, reversing
    changes made to 6421ce829d27d17255ecdb482b2d2df676b57473.

--- lib/features/explore/presentation/views/explore_view.dart
+++ lib/features/explore/presentation/views/explore_view.dart
@@ -5,10 +5,9 @@ import 'package:flutter_task10_team_housely_app_beg/core/services/service_locato
 import 'package:flutter_task10_team_housely_app_beg/features/auth/data/data_sources/auth_local_data_source.dart';
 import 'package:flutter_task10_team_housely_app_beg/features/select_location/data/manager/location_cubit.dart';
 import 'package:latlong2/latlong.dart';
-
 import 'widgets/explore_view_body.dart';
 
-class ExploreView extends StatefulWidget {
+class ExploreView extends StatelessWidget {
   final LatLng? initialLocation;
   final VoidCallback onBackToHome;
 
@@ -19,97 +18,16 @@ class ExploreView extends StatefulWidget {
   });
 
   @override
-  State<ExploreView> createState() => _ExploreViewState();
-}
-
-class _ExploreViewState extends State<ExploreView>
-    with WidgetsBindingObserver {
-  late final LocationCubit _locationCubit;
-
-  @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance.addObserver(this);
-
-    _locationCubit = LocationCubit(
-      getIt<LocationService>(),
-      getIt<AuthLocalDataSource>(),
-    );
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _checkLocationService();
-    });
-  }
-
-  Future<void> _checkLocationService() async {
-    final locationService = getIt<LocationService>();
-
-    final enabled = await locationService.isLocationServiceEnabled();
-
-    if (!mounted) return;
-
-    if (enabled) {
-      _locationCubit.initialize(loadNearby: true);
-      return;
-    }
-
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (dialogContext) {
-        return AlertDialog(
-          title: const Text('Location Service Disabled'),
-          content: const Text(
-            'Please enable Location Services (GPS) to view nearby places and properties.',
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(dialogContext);
-              },
-              child: const Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                Navigator.pop(dialogContext);
-                await locationService.openLocationSettings();
-              },
-              child: const Text('Open Settings'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) async {
-    if (state == AppLifecycleState.resumed) {
-      final enabled =
-          await getIt<LocationService>().isLocationServiceEnabled();
-
-      if (enabled) {
-        _locationCubit.initialize(loadNearby: true);
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    _locationCubit.close();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: _locationCubit,
+    return BlocProvider(
+      create: (_) => LocationCubit(
+        getIt<LocationService>(),
+        getIt<AuthLocalDataSource>(),
+      )..initialize(loadNearby: true), // جلب الموقع والأماكن المجاورة فور الفتح
       child: Scaffold(
         extendBodyBehindAppBar: true,
         body: ExploreViewBody(
-          onBackToHome: widget.onBackToHome,
+          onBackToHome: onBackToHome,
         ),
       ),
     );
